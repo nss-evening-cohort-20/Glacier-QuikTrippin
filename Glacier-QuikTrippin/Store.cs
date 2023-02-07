c58bf3164234f8ed6d86137867f0e86bb0298b88
Jeremy White <jcwhite414@gmail.com>
2023-02-06 21:18:00 -0600

    Basic Store class created

---
+++ Glacier-QuikTrippin/Store.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glacier_QuikTrippin;
+
+public class Store
+{
+   public int Number { get; set; }
+   public int GasYearly { get; set; }
+   public int GasCurrentQuarter { get; set; }
+   public int RetailYearly { get; set; }
+   public int RetailCurrentQuarter { get;set; }
+
+    Dictionary<string, List<int>>  StoreEmployees{ get;}
+
+    public Store() {
+    StoreEmployees= new Dictionary<string, List<int>>();
+    }
+}
