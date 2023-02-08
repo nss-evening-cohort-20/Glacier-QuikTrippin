50393192454247a1dd87855ba0d9b94c197bc38b
Jeremy White <jcwhite414@gmail.com>
2023-02-07 20:42:20 -0600

    Added region for testing store builder

--- Glacier-QuikTrippin/Program.cs
+++ Glacier-QuikTrippin/Program.cs
@@ -1,6 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Glacier_QuikTrippin;
 
-StoreBuilder bldr = new StoreBuilder();
-bldr.Run();
-Console.ReadLine();
+//Commands for accessing store buillder and Store Sales Editor
+
+#region Testing Store Builder and Sales Number Editor
+//StoreRepository repo = new StoreRepository();
+//StoreBuilder bldr = new StoreBuilder();
+//bldr.Run();
+
+//StoreSalesEditor edt = new StoreSalesEditor();
+//Store currentStore = repo.GetStoreByNumber(250);
+//edt.Run(currentStore);
+#endregion
