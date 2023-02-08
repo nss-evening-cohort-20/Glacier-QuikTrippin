c4d5ac6cff2c9049c138daea4ca394c7786e5826
r-stroud <stroudrobert86@gmail.com>
2023-02-07 20:52:30 -0600

    Added Menu class and classes related to districts

---
+++ Glacier-QuikTrippin/District.cs
@@ -0,0 +1,18 @@
+﻿namespace Glacier_QuikTrippin;
+
+public class District
+{
+    public int Id { get; }
+
+    public DistrictManager DistrictManager { get; set; }
+
+
+    public District(DistrictRepository districtRepository) 
+    {
+    
+        Id = districtRepository.GetId();
+        districtRepository.SaveNewDistrict(this);
+    }
+
+ 
+}
--- Glacier-QuikTrippin/Program.cs
+++ Glacier-QuikTrippin/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Glacier_QuikTrippin;
+
 Console.WriteLine("test1");
+DistrictRepository repository= new DistrictRepository();
+District test =new District(repository);
+District test2=new District(repository);
+District test3=new District(repository);
+var repo = repository.GetDistricts();
+foreach (var item in repo)
+{
+    Console.WriteLine(item.Id);
+}
+
+Title title = new Title();
+title.DisplayTitle();
+
+string prompt = "Please use the arrows keys to navigate the menu. Press enter to make a selection.";
+string[] options= { "option1", "option2", "option3" };
+
+Menu mainMenu = new Menu(prompt, options);
+mainMenu.Run();
+
+while (true)
+{
+
+}
