1d4861c4de5cab47005dae5f4aeccf1bb497fadc
Jeremy White <jcwhite414@gmail.com>
2023-02-10 22:05:02 -0600

    Added title

---
+++ Glacier-QuikTrippin/DistrictDashboard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glacier_QuikTrippin;
+
+public class DistrictDashboard
+{
+    public int Run(int districtNumber)
+    {
+        Console.WriteLine($"District Dashboard: {districtNumber}");
+        string prompt = "Please use the arrows keys to navigate the menu. Press enter to make a selection.";
+        string[] options = { "Add a Store", "Store Dashboard", "Generate District Report", "Back to District Selection" };
+
+        Menu DistrictDashboardMenu = new Menu(prompt, options);
+
+        int userChosenOption = DistrictDashboardMenu.Run();
+
+        switch (userChosenOption)
+        {
+            case 0:
+                return 0;
+            case 1:
+                return 1;//find a way to choose the next thing to run
+            case 2: return 2;
+            default: return 3;
+        }
+
+
+    }
+}
